1508cbd142c275a19e463c0f245da898511016da
Erwan Lu <dararith.lu@gmail.com>
2026-01-22 18:41:18 +0100

    Add commentaries to PlayerMovementController script

--- Assets/Components/Player/Scripts/PlayerMovementController.cs
+++ Assets/Components/Player/Scripts/PlayerMovementController.cs
@@ -68,21 +68,34 @@ public class PlayerMovementController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Handle slide input between lanes.
+    /// Check the "_isSliding" condition for debugging.
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
     private IEnumerator Coroutine_Slide(Transform target)
     {
         _isSliding = true;
         
+        // Set to 0 a timer for sliding.
         var _slideTimer = 0f;
         
+        // Loop who runs till the timer is not equal to the slide's duration.
         while (_slideTimer < _slideDuration)
         {
 
+            // _slideTimer increase as time flies.
+            // Set a variable to normalize time with a value between 0 and 1 to avoid potential errors.
+            // Set the target position with the x position of the coroutine's argument -> "target".
             _slideTimer += Time.deltaTime;
             var normalizedTime = Mathf.Clamp01(_slideTimer / _slideDuration);
             var targetPosition = new Vector3 (target.position.x, transform.position.y, transform.position.z);
 
+            // Movement between two positions in a defined time.
             transform.position = Vector3.Lerp(transform.position, targetPosition, normalizedTime);
         
+            // Wait for the next frame.
             yield return null;
         }
         
