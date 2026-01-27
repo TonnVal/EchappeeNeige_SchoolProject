fab1a2c85dcca1ac0cc646c6b1fdbf412667e10d
Erwan Lu <dararith.lu@gmail.com>
2026-01-27 18:40:33 +0100

    Refacto Game Event Service and scoring system
    • Score increase by time like previously.
    • Game Event System manage scoring system and game states.

--- Assets/Components/StateMachine/Scripts/GameState.cs
+++ Assets/Components/StateMachine/Scripts/GameState.cs
@@ -1,22 +1,39 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
 namespace Components.StateMachine
 {
     public class GameState : State
     {
+        [Header("Play Game Parameters")]
+        private float _currentTime = 0f;
+        private float _currentScore = 0f;
+        private int _currentMultiplior = 5;
+        
         public GameState(StateMachine stateMachine) : base(stateMachine) { }
-
+        
         public override void Enter()
         {
-            // noop
+            GameEventService.OnGameState?.Invoke(true);
+            GameEventService.OnScoreIncrease += ScoreIncreasing;
+        }
+
+
+        public override void Update()
+        {
+            GameEventService.OnScoreIncrease?.Invoke(_currentScore);
         }
 
         public override void Exit()
         {
-            // noop
+            GameEventService.OnGameState?.Invoke(false);
         }
-
-        public override void Update()
+        
+        private void ScoreIncreasing(float obj)
         {
-            // noop
+            _currentTime += Time.deltaTime;
+            _currentScore = _currentTime * _currentMultiplior;
         }
     }
 }
--- Assets/Components/UIMainGameController/Scripts/UIScore.cs
+++ Assets/Components/UIMainGameController/Scripts/UIScore.cs
@@ -5,6 +5,16 @@ public class UIScore : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
 
+    private void Start()
+    {
+        GameEventService.OnScoreIncrease += SetScore;
+    }
+
+    private void OnDestroy()
+    {
+        GameEventService.OnScoreIncrease -= SetScore;
+    }
+
     private void SetScore(float score)
     {
         _scoreText.text = "Score: " + score.ToString("0");
